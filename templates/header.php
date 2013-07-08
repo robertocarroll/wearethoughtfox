c96dff9f128c760ba966786b051f0b8208ca614b
robertocarroll <robertocarroll@gmail.com>
2013-07-08 21:13:13 +0100

    Fixed up the nav so it centres

--- templates/header.php
+++ templates/header.php
@@ -16,14 +16,15 @@
 
 <?php } ?>    
   
-  
-    <nav class="nav-main" role="navigation">
+  <div class="nav-wrapper">
+    <nav class="nav-main wrapper" role="navigation">
       <?php
         if (has_nav_menu('primary_navigation')) :
           wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
         endif;
       ?>
     </nav>
+  </div>
 
 </header>
 
