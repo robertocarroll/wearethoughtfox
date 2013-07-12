8e0bc895cc8bb97860e4197e2ddab86a37e25a52
robertocarroll <robertocarroll@gmail.com>
2013-07-12 20:34:57 +0100

    Success with the nav fix on the homepage.

--- assets/javascripts/watf.js
+++ assets/javascripts/watf.js
@@ -23,41 +23,28 @@ $(document).ready(function(){
 
  */
 
-$(document).ready(function() {
+if($('body').hasClass('home')){ 
+  var $filter = $('.nav-wrapper'); 
+    
+
+if ($filter.size()) 
+  { 
+    $(window).scroll(function () 
+
+    { 
+    var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+    var splash = $('.splash').height(); // the height of the nav from the top
   
-if($('body').hasClass('home')){
+      if (!$filter.hasClass('fix') && scroll_top > $filter.offset().top) 
+      {       
+        $filter.addClass("fix");
+      } 
 
- $(function() {
- 
-    // grab the initial top offset of the navigation 
-    var sticky_navigation_offset_top = $('.nav-wrapper').offset().top;
-     
-    // our function that decides weather the navigation bar should have "fixed" css position or not.
-    var sticky_navigation = function(){
-        var scroll_top = $(window).scrollTop(); // our current vertical position from the top
-         
-        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-        // otherwise change it back to relative
-        if (scroll_top > sticky_navigation_offset_top) { 
-            $('.nav-wrapper').addClass("fix");
-        } else {
-            $('.nav-wrapper').removeClass("fix");
-        }   
-    };
-     
-    // run our function on load
-    sticky_navigation();
-     
-    // and run it again every time you scroll
-    $(window).scroll(function() {
-         sticky_navigation();
-    });
- 
-});
-
+      else if ($filter.hasClass('fix') && scroll_top < splash) 
+      { 
+        $filter.removeClass("fix");
+      } 
+    }); 
+  } 
 
 }
-
-});
-
-
