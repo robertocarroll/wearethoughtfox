fbfa9e261cc132c4b5ce01f8702f4f261fd78c41
robertocarroll <robertocarroll@gmail.com>
2013-07-17 11:51:39 +0100

    Updated the menu to work a bit better (logo was overlapping nav when position:fixed).

--- assets/javascripts/watf.js
+++ assets/javascripts/watf.js
@@ -1,6 +1,6 @@
 /* 
  * For the smooth scroll http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
- */
+ 
 
 $(document).ready(function(){
   $('a[href^="#"]').on('click',function (e) {
@@ -17,11 +17,21 @@ $(document).ready(function(){
   });
 });
 
+*/
+
 /* 
  * For the highlighting the nav http://trevordavis.net/blog/jquery-one-page-navigation-plugin
  */
 
-
+ $('.nav').onePageNav({
+      currentClass: 'current-menu-item',
+      changeHash: false,
+      scrollSpeed: 750,
+      scrollOffset: 2,
+      scrollThreshold: 0,
+      filter: '',
+      easing: 'swing'
+  });
 
 
 /* 
@@ -29,54 +39,36 @@ $(document).ready(function(){
 
  */
 
-if($('body').hasClass('home')){ 
-  var $filter = $('.nav-wrapper'); 
-    
+if($('body').hasClass('home')){
+  var $filter = $('.nav-wrapper');
 
-if ($filter.size()) 
-  { 
-    $(window).scroll(function () 
+  if ($filter.size())
+  {
+    $(window).scroll(function ()
 
-    { 
+    {
     var scroll_top = $(window).scrollTop(); // our current vertical position from the top
     var splash = $('.splash').height(); // the height of the nav from the top
-  
-      if (!$filter.hasClass('fix') && scroll_top > $filter.offset().top) 
-      {       
+
+      if (!$filter.hasClass('fix') && scroll_top > $filter.offset().top)
+      {
         $filter.addClass("fix");
         $(".logo-small").addClass("fix-logo");
-        
-        $('.nav').onePageNav({
-      currentClass: 'current-menu-item',
-      changeHash: false,
-      scrollSpeed: 750,
-      scrollOffset: 0,
-      scrollThreshold: 0,
-      filter: '',
-      easing: 'swing',
-      begin: function() {
-          //I get fired when the animation is starting
-      },
-      end: function() {
-          //I get fired when the animation is ending
-      },
-      scrollChange: function($currentListItem) {
-          //I get fired when you enter a section and I pass the list item of the section
+
       }
-  });
 
-      } 
+      else if ($filter.hasClass('fix') && scroll_top < splash)
+      {
 
-      else if ($filter.hasClass('fix') && scroll_top < splash) 
-      { 
-        $filter.removeClass("fix");
+       $filter.removeClass("fix");
         $('.nav').onePageNav({
             filter: ':not(.our-work)'
         });
+
         $(".logo-small").removeClass("fix-logo");
         $(".our-work").removeClass("current-menu-item");
-      } 
-    }); 
-  } 
+      }
+    });
+  }
 
 }
