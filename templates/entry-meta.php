c1e6b81384d9a7f0b6edd11ee1a50682c285a6a9
robertocarroll <robertocarroll@gmail.com>
2013-07-23 14:34:22 +0100

    added comma to tag list on blog pagewq

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -3,6 +3,6 @@
 	<li><time class="date" datetime="<?php echo get_the_time('c'); ?>" pubdate><?php echo get_the_date('j F Y'); ?></time></li>
 </ul>
 			
-<?php the_tags('<span class="inline">See also:&nbsp;</span> <ul class="entry-tags nav"><li>','</li><li>','</li></ul>'); ?>
+<?php the_tags('<span class="inline">See also:&nbsp;</span> <ul class="entry-tags nav"><li>','</li>, <li>','</li></ul>'); ?>
 
 	
